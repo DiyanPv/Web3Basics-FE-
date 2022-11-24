7f05c571c3b501df929967842fe64a21fc700520
DiyanPv <diyanpetrov61@gmail.com>
2022-11-24 11:30:40 +0200

    Connect button functionality added/slight css tweaks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { HomePageComponents } from "./components/Home/Home";
+import { useState } from "react";
 function App() {
+  const [connected, isConnected] = useState(false);
+  const connect = async () => {
+    if (window.ethereum) {
+      console.log(`Metamask detected`);
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      isConnected(true);
+      console.log(`Metamask connected!`);
+    } else {
+      console.log(`Metamask not detected`);
+    }
+  };
+
   return (
     <div className="homepage">
-      <button className="btn-connect">Connect Wallet</button>
+      <button className="btn-connect" onClick={connect}>
+        {connected ? `Connected` : `Connect Wallet`}
+      </button>
       <h1 className="title">Funding the contract</h1>
       <HomePageComponents />
     </div>
